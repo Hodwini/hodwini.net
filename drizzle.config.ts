3f6e8b28f7f9f90ab5660461646bcec5627b4063
nsvk13 <nsvkdev@gmail.com>
2025-10-27 06:33:35 +0300

    del for refactor

--- drizzle.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-defineConfig({
-    dialect: 'postgresql',
-    schema: './server/database/schemas/*',
-    out: './drizzle'
-})
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { app } from './api/index'
-
-const port = Number(process.env.PORT ?? 3000)
-app.listen(port)
-console.log(`http://localhost:${port}`)
